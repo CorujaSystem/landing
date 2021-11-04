65bfb69d93df29db0672ed3df6f0b572e6768ff7
José Eduardo <josee.edumont@gmail.com>
2021-11-04 13:37:56 -0300

    setup twind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
+import { tw } from 'twind'
 
 const Home: NextPage = () => {
   return (
-    <h1>
+    <h1 className={tw`text-purple-800`}>
       Hello Breno
     </h1>
   )
